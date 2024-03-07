3164729e87553859de9aeb6bb120f3c862bf372f
OliverVlz <Oliver.vlz01@gmail.com>
2024-03-07 14:03:31 -0500

    Created seeds file sample data

--- TestingTwo/db/seeds.rb
+++ TestingTwo/db/seeds.rb
@@ -1,9 +1,29 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+10.times do |blog|
+    Blog.create!(
+        title: "My Blog Post #{blog}",
+        body: "fasdmfkl flsakdmflaks asdlfkmasdkfm fldsakmflkma aslkdfmsdkmlkrt"
+    )
+
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15
+    )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item}",
+        subtitle: "My great service",
+        body: "holasi pero como es qu esto funciona",
+        main_image: "http://via.placeholder.com/400x200",
+        thumb_image: "http://via.placeholder.com/300x100"
+    )
+end
+puts "9 portfolio items created"
